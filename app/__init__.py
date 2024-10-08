c6d99ff720b7071485a94e4d7f1395f4ca543686
ahmedhelalley <a.helalley@gmail.com>
2024-10-08 17:27:17 +0300

    enabled CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Enable CORS
+    CORS(app)
+
     from app.routes import bp as routes_bp
     app.register_blueprint(routes_bp)
 
     return app
-
